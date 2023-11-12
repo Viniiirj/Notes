c91d389536050ed8fc0b97a4434bce95df3d0a61
viniiirj <vsr.rio@gmail.com>
2023-11-11 22:18:40 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/global';
-import theme from './styles/theme'
-import { Routes } from '../routes'
-import { AuthProvider } from '../hooks/auth'
+import GlobalStyles from './pages/styles/global';
+import theme from './pages/styles/theme'
+import { Routes } from './routes'
+import { AuthProvider } from './hooks/auth'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
