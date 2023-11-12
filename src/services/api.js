3fd223997e7b3920dff4d0020cbd52e9a6c4b9bf
viniiirj <vsr.rio@gmail.com>
2023-11-11 22:50:51 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://rocketnotes-api-yx5r.onrender.com"
+    // baseURL: "https://rocketnotes-api-yx5r.onrender.com"
+    baseURL: 'https://rocketnotes-api-back-end.onrender.com'
 })
